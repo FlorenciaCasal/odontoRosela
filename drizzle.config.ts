cab0f2b024e443e18ff136b2045a6a91025cbfc8
FlorenciaCasal <florenciacasal88.fc@gmail.com>
2025-09-04 14:52:30 -0300

    cambie postgres_url por postgres_postgres_url

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -5,7 +5,7 @@ export default defineConfig({
   schema: "./src/lib/schema.ts",
   dialect: "postgresql",
   dbCredentials: {
-    url: process.env.POSTGRES_URL!
+    url: process.env.POSTGRES_POSTGRES_URL!
   },
   verbose: true,
   strict: true
